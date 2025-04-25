ad6e6c26ae97805b4250f6ec8c03ac637d668bff
Luis Alejandro Taborda Andrade <83258732+Lataborda@users.noreply.github.com>
2025-04-25 15:35:52 -0500

    Update opennesstool.py

--- opennesstool.py
+++ opennesstool.py
@@ -191,4 +191,4 @@ if st.button("Calcular puntuaciones"):
 st.divider()
 st.markdown('*Copyright (C) 2025. CIRAD*')
 st.caption('**Authors: Alejandro Taborda A., (latabordaa@unal.edu.co), Chloé Lecombe**')
-st.image('nueva-carpeta/aperto.png')
+st.image('Imange 1.png')
